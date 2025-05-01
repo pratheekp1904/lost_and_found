f34bb5ba9a13bcb4393b5662357716241491d384
Pratheek <pratheekpganiga@gmail.com>
2025-05-02 02:04:13 +0530

    Better Login Function

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
 // server.js
 
 // Load environment variables
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
 
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
