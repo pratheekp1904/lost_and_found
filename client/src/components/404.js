a8f768ebf2fa1e92e7cee515f7b1a63c914de4f0
Pratheek <pratheekpganiga@gmail.com>
2025-05-01 22:59:13 +0530

    testing push

--- client/src/components/404.js
+++ client/src/components/404.js
@@ -8,7 +8,7 @@ const NotFound = () => {
       <div className="not-found">
         <h1 className="Error-404">Error-404 :(</h1>
         <p className="error-404-content">
-          Oops! The page you're looking for could not be found.
+          Oops!! The page you're looking for could not be found.
         </p>
         <NavLink to="/">
           <button className="back-to-home">Back to Home</button>
